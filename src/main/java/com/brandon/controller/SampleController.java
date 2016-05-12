d70557c93b0d0f2919638602f719b299a2d4457c
Brandon <the31streaper@Brandons-MacBook-Air.local>
2016-05-12 09:38:13 -0700

    Initial Commit

---
+++ src/main/java/com/brandon/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.brandon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+	
+	@RequestMapping("home")
+	public String loadHomePage(Model m) {
+		m.addAttribute("name", "Brandon");
+		return "home";
+	}
+
+}
